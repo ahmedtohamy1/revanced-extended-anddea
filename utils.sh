cd72e1c91fcd588648d4d79aa0105dfe880d979d
Ahmed Tohamy <1ahmed.tohamy@gmail.com>
2024-05-25 03:36:32 +0300

    Update utils.sh

--- utils.sh
+++ utils.sh
@@ -119,7 +119,7 @@ config_update() {
 			fi
 		else
 			sources[$PATCHES_SRC]=0
-			if ! last_patches_url=$(gh_req "https://api.github.com/repos/anddea/revanced-patches/releases/tags/v2.225.0-dev.21" - \
+			if ! last_patches_url=$(gh_req "https://api.github.com/repos/anddea/revanced-patches/releases/156668825" - \
 				| jq -e -r '.assets[] | select(.name | endswith("jar")) | .name'); then
 				abort oops
 			fi
